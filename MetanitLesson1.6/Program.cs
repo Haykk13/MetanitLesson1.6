a8b013a08121e6c3d7103a4d87ee81829f67109b
Hayk <hayk.hayr3@gmail.com>
2020-06-28 01:20:09 +0400

    Add project files.

---
+++ MetanitLesson1.6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MetanitLesson1._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the operation number: 1. Addition 2. Subtraction 3. Multiplication: ");
+            int operation = int.Parse(Console.ReadLine());
+            
+            switch (operation)
+            {
+                case 1:
+                    Console.WriteLine("Addition"); 
+                    break;
+                case 2:
+                    Console.WriteLine("Subtraction");
+                    break;
+                case 3:
+                    Console.WriteLine("Multiplication");
+                    break;
+                default:
+                    Console.WriteLine("Operation is undefined");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
